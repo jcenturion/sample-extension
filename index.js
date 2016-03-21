d8b4cc31cc3b5066ff3f039d5acc6b382bfb0ac2
Javier Centurion <centurion.javier@gmail.com>
2016-03-21 19:26:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 module.exports = function (cb) {
   cb(null, 'Welcome to Sample Webtask!');
 }
+
+module.exports.meta = {
+  title:       "Sample Webtask",
+  name:        "sample-webtask",
+  version:     "1.0.0",
+  author:      "javier.centurion@auth0.com",
+  description: "This is an example about how to use webtasks",
+  type:        "application",
+  repository:  "https://github.com/jcenturion/sample-webtask",
+  keywords: [
+    "auth0",
+    "extension"
+  ]
+}
