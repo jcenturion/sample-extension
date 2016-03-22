7d9f5e102c45bb10b998c92c4656a15d6635f3cc
jcenturion <javier.centurion@auth0.com>
2016-03-22 20:48:40 -0300

    Removing module.exports.meta

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express  = require('express');
 var app      = express();
 var template = require('./views/index.jade');
+var metadata = require('./webtask.json');
 
 app.get('/', function (req, res) {
   res.header("Content-Type", 'text/html');
   res.status(200).send(template());
+});
+
+// This endpoint would be called by webtask-gallery to dicover your metadata
+app.get('/meta', function (req, res) {
+  res.status(200).send(metadata);
 });
 
 module.exports = app;
--- webtask.js
+++ webtask.js
@@ -2,5 +2,4 @@ var Webtask = require('webtask-tools');
 
 // This is the entry-point for the Webpack build. We need to convert our module
 // (which is a simple Express server) into a Webtask-compatible function.
-module.exports      = Webtask.fromExpress(require('./index.js'));
-module.exports.meta = require('./webtask.json');
+module.exports = Webtask.fromExpress(require('./index.js'));
